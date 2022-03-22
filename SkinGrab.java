dac1690f512bfa91f7ae1443dcf21d8ffabb3c3f
boni! <74261263+boniikawaiidesu@users.noreply.github.com>
2022-03-21 22:21:20 -0600

    Create SkinGrab.java

---
+++ SkinGrab.java
@@ -0,0 +1,40 @@
+package yourLocation;
+
+import java.util.*;
+import java.net.URL;
+import java.util.List;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+
+public class SkinGrab {
+	public static void DownloadImg(String skin_name){
+		BufferedImage hskin = null;
+        //RestrainMod.logger.info("thread downloader has started....");
+        //RestrainMod.logger.info("For player: " + skin_name + " fetch");
+        //RestrainMod.logger.info("https://minecraftskinstealer.com/api/v1/skin/download/skin/"+skin_name);
+        try{
+            //URL url = new URL("http://minecraftskinstealer.com/api/v1/skin/download/skin/"+skin_name);
+        	String page_url = "https://minecraft.tools/download-skin/"+skin_name; 
+        	URL url = new URL(page_url);
+        	File directory = new File("HotCache");
+        	if(! directory.exists()){
+        	   directory.mkdir();
+        	}
+            hskin = ImageIO.read(url);
+            String file_name = skin_name+"_skin.png";
+            File img_out = new File(directory,file_name);
+            ImageIO.write(hskin, "png",img_out);
+        }catch(IOException e){
+            //RestrainMod.logger.info("Error Downloading skin for player: "+skin_name);
+            e.printStackTrace();
+            
+        }
+    }
+	
+	public static void main(String[] args) {
+		System.out.println("hello test");
+		DownloadImg("BoniiKwiiDz");		
+	}
+}
